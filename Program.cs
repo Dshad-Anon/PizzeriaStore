1a38f6f6863dda1d0aa87e78e32ff4af100886ef
Shiva Adhikari <dshad002@edu.xamk.fi>
2024-12-13 05:31:31 +0200

    MemoryInDatabase changed to Sqlite and Migrations added via commands.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using Pizzeria.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source = Pizzas.db";
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDB>(connectionString);
 builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new OpenApiInfo{
         Title = "Pizzeria Store",
